f9329d9d8ca0633e738eecfafe2c1da88ef6da82
Charles-Thomson <65188045+Charles-Thomson@users.noreply.github.com>
2023-09-05 13:15:13 +0100

    Testing basic functunality

---
+++ application/ann/agents/agent_generator.py
@@ -0,0 +1,35 @@
+"""Generator for agent"""
+from typing import Generator
+
+from application.ann.agent_brains.brain_factory import BrainFactory
+
+from application.ann.agents.agent_factory import AgentFactory
+
+
+def new_agent_generator(
+    ann_config: dict,
+    agent_type: str,
+    parents: list,
+    generation_size: int,
+    generation_number: int,
+) -> Generator:
+    """Test"""
+    for _ in range(generation_size):
+        if generation_number == 0:
+            agent_brain: object = BrainFactory.make_brain(
+                brain_type="random_weighted_brain",
+                ann_config=ann_config,
+                parents=parents,
+            )
+        else:
+            agent_brain: object = BrainFactory.make_brain(
+                brain_type="generational_weighted_brain",
+                ann_config=ann_config,
+                parents=parents,
+            )
+
+        agent: object = AgentFactory.make_agent(
+            agent_type=agent_type, brain=agent_brain
+        )
+
+        yield agent
