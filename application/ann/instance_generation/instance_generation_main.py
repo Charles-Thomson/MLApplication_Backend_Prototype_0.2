91f5cc845ed180ed057a48a87b51afce246e8035
Charles-Thomson <65188045+Charles-Thomson@users.noreply.github.com>
2023-10-09 03:43:05 +0100

    Completed learning instance tests

--- application/ann/instance_generation/instance_generation_main.py
+++ application/ann/instance_generation/instance_generation_main.py
@@ -7,6 +7,7 @@ from operator import attrgetter
 from functools import partial
 
 from application.ann.environments.environment_factory import EnvironmentFactory
+from application.ann.agent_brains.static_state_brain import BrainInstance
 
 from application.ann.agents.agent_generator import new_agent_generator
 
@@ -27,7 +28,6 @@ from application.ann.logging_files.logging_deco import (
 # TODO: Instance generation:
 # Need to save the highest fitness brain from each instance run
 
-
 # TODO: Set up the Django DB model to store the highest fitness from each instance
 
 
@@ -38,7 +38,7 @@ class Learning_Instance:
     then be used on a new environment
     """
 
-    def __init__(self, id, agent_generator: object, instance_config: dict):
+    def __init__(self, id, agent_generater_partial: object, instance_config: dict):
         self.instance_id: str = id
 
         self.current_fitness_threshold: float = instance_config["fitness_threshold"]
@@ -49,26 +49,29 @@ class Learning_Instance:
         ]
 
         self.max_generation_size: int = instance_config["max_generation_size"]
-        self.agent_generator: callable = agent_generator  # new per generation
+        self.agent_generater_partial: callable = (
+            agent_generater_partial  # new per generation
+        )
 
-        self.current_parents: list = []
+        self.current_parents: list[BrainInstance] = []
 
         self.new_generation_threshold: int = instance_config["new_generation_threshold"]
 
-        self.brains = []
+        self.brains: list[BrainInstance] = []
+        self.longest_path_brains: list[BrainInstance] = []
+        self.highest_fitness_brains: list[BrainInstance] = []
 
     def run_instance(self):
         """run the instance"""
         current_generation_number: int = 0
         # new_fitness_threshold = self.current_fitness_threshold
-        new_parents: list = []
+        new_parents: list[BrainInstance] = []
 
         for current_generation_number in range(self.max_number_of_generations):
             new_fitness_threshold = self.generate_new_fitness_threshold(new_parents)
 
-            agent_generator: object = self.agent_generator(
-                parents=new_parents,
-                max_generation_size=self.max_generation_size,
+            agent_generator: object = self.get_new_agent_generator(
+                new_parents=new_parents,
                 current_generation_number=current_generation_number,
             )
 
@@ -80,23 +83,59 @@ class Learning_Instance:
             if len(new_parents) <= self.current_generation_failure_threshold:
                 break
 
-            self.get_highest_fitness_brain(new_parents)
+            brain_highest_fitness = self.get_highest_fitness_brain(new_parents)
+            brain_longest_path = self.get_longest_path_brain(new_parents)
 
-        # TODO: Get the longest path brain from each generation
+            self.highest_fitness_brains.append(brain_highest_fitness)
+            self.longest_path_brains.append(brain_longest_path)
 
         # For logging deco
         return self.brains
 
-    # TODO: Write a test with logging to track the highest fitness brain
-    def get_highest_fitness_brain(self, parents: list[object]) -> object:
+    def get_new_agent_generator(
+        self,
+        new_parents: list,
+        current_generation_number: int,
+    ) -> Generator:
+        """Create a new agenet generator
+        var: Parents - Parnets for the new generation
+        var: current_generation_number - the current generation
+        rtn: Agent generator
+        """
+
+        return self.agent_generater_partial(
+            parents=new_parents,
+            max_generation_size=self.max_generation_size,
+            current_generation_number=current_generation_number,
+        )
+
+    def get_highest_fitness_brain(self, parents: list[BrainInstance]) -> object:
         """
         Get the highest fitness brain from the list of parents
+        var: parents - the given range of brains
+        rtn: highest_fitness_brain - the brain instance with the highest fitness
         """
         if not parents:
             return None
         highest_fitness_brain = max(parents, key=attrgetter("fitness"))
 
-        print(highest_fitness_brain)
+        return highest_fitness_brain
+
+    def get_longest_path_brain(self, parents: list[BrainInstance]) -> object:
+        """
+        Get the longest path brain instances
+        var: parents - the given range of brains
+        rtn: longest_path_brain - the brain instance with the longest path
+        """
+
+        if not parents:
+            return None
+        longest_path_brain = parents[0]
+        for brain in parents:
+            if len(brain.traversed_path) > len(longest_path_brain.traversed_path):
+                longest_path_brain = brain
+
+        return longest_path_brain
 
     def run_generation(
         self, agent_generator: Generator, fitness_threshold: float
@@ -123,7 +162,7 @@ class Learning_Instance:
         return new_parents
 
     # TODO: Move fitness threshold logging to the testing side
-    @with_fitness_threshold_logging
+    # @with_fitness_threshold_logging
     def generate_new_fitness_threshold(self, parents: list[object]) -> float:
         """
         Calculate a new fitness threshold based on the average fitness + 10%
@@ -157,7 +196,7 @@ def new_instance(config: json) -> Learning_Instance:
         env_type=config["env_type"], config=env_config
     )
 
-    agent_generater: callable = partial(
+    agent_generater_partial: callable = partial(
         new_agent_generator,
         ann_config=ann_config_formatted,
         agent_type=config["agent_type"],
@@ -168,7 +207,7 @@ def new_instance(config: json) -> Learning_Instance:
 
     this_instance = Learning_Instance(
         id=id,
-        agent_generator=agent_generater,
+        agent_generater_partial=agent_generater_partial,
         instance_config=instance_config_formatted,
     )
 
